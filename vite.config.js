efc03793d9db4c659c8a3d99eaf5b207708c78e0
Suryansh9304 <rajputana932004@gmail.com>
2025-11-11 19:06:26 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// **CRUCIAL FIX:** Sets the base path for assets and scripts.
-const REPO_NAME = 'Portfolio'; 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: `/${REPO_NAME}/`, 
+  base: '/my-portfolio/', // ✅ must exactly match your GitHub repo name (lowercase)
   plugins: [react()],
 })
